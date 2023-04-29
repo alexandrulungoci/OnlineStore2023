c5e34853898b17ba55bed6df06e0d21c0e7d3ad8
AlexLungoci <alexandru.lungoci@yahoo.com>
2023-04-30 00:04:58 +0300

    initial commit

---
+++ src/test/java/com/alex/onlinestore2023/Onlinestore2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.onlinestore2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onlinestore2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
